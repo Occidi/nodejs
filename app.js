768bcb4fc72dff15db810503c0070a5f8df0208f
Gwendal Jeanson <gwendal.jeanson@yahoo.fr>
2017-03-01 20:34:16 +0100

    handling post request

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
+
+// create application/x-www-form-urlencoded parser 
+const urlencodedParser = bodyParser.urlencoded({ extended: false })
 
 app.set('view engine', 'ejs');
 app.use('/assets', express.static('assets'));
@@ -9,9 +13,13 @@ app.get('/', function(req, res) {
     res.render('index');
 });
 
-app.get('/contact', function(req, res) {
-    const contact = req.query;
-    res.render('contact', {contact : contact});
+app.get('/contact',  function(req, res) {
+    res.render('contact', {contact : req.query});
+});
+
+app.post('/contact', urlencodedParser, function(req, res) {
+    console.log(req.body);
+    res.render('contact-success', {data: req.body});
 });
 
 app.get('/profile/:id', function(req, res) {
